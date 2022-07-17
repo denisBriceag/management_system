46bb16ae915225d7e1d0b8e8973f64edbde32cfe
denisBriceag <denisbriceag21@gmail.com>
2022-07-17 18:12:25 +0300

    certain changes

--- src/app/core/maindashboard/maindashboard.component.ts
+++ src/app/core/maindashboard/maindashboard.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { Actions, ofActionDispatched } from '@ngxs/store';
 import { Login } from 'src/app/auth/state/auth.actions';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Router } from '@angular/router';
 
-@UntilDestroy()
 @Component({
   selector: 'app-maindashboard',
   templateUrl: './maindashboard.component.html',
@@ -14,10 +12,8 @@ export class MaindashboardComponent implements OnInit {
   constructor(private actions: Actions, private router: Router) {}
 
   ngOnInit(): void {
-    this.actions
-      .pipe(untilDestroyed(this), ofActionDispatched(Login))
-      .subscribe(() => {
-        this.router.navigate(['/dashboard']);
-      });
+    this.actions.pipe(ofActionDispatched(Login)).subscribe(() => {
+      this.router.navigate(['/dashboard']);
+    });
   }
 }
